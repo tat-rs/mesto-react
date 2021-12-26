aefef32578217b3c667b0caa369c05fe4cafc082
Tatiana Roslyakova <tat-rs@yandex.ru>
2021-12-26 23:47:14 +0300

    отображается иконка удаления и лайка

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,35 @@
 import React from "react";
+import { CurrentUserContext } from '../contexts/CurrentUserContext'
 
 function Card(props) {
-  //сохранение данных карточки при клике
-  function handleClick() {
+
+   //сохранение данных карточки при клике
+   function handleClick() {
     props.onCardClick({
       name: props.card.name, 
       link: props.card.link
     })
   }
+
+  const currentUserData = React.useContext(CurrentUserContext); // подписались на контекст текущих данных пользователя
+  
+  const isOwner = props.card.owner._id === currentUserData._id; // определяем является ли пользователем текущим
+
+  const cardDeleteButtonClassName = (`cards__delete ${isOwner ? 'cards__delete_visible' : ''}`); // определяем класс кнопки удаления
+
+  const isLiked = props.card.likes.some(i => i._id === currentUserData._id); // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
+
+  const cardLikeButtonClassName = (`cards__button ${isLiked ? 'cards__button_active' : ''}`); // определяем класс для кнопки лайка
   
   return (
     <>
       <li className='cards__item'>
-        <button className='cards__delete' type='button' aria-label='Удалить'></button>
+        <button className={cardDeleteButtonClassName} type='button' aria-label='Удалить'></button>
         <img className='cards__image' src={`${props.card.link}`} alt={`${props.card.name}`} onClick={handleClick} />
         <div className='cards__desc'>
           <h2 className='cards__subtitle'>{props.card.name}</h2>
           <div className='cards__container-likes'>
-            <button className='cards__button' type='button' aria-label='Лайк'></button>
+            <button className={cardLikeButtonClassName} type='button' aria-label='Лайк'></button>
             <p className='cards__sum-likes'>{props.card.likes.length}</p>
           </div>
         </div>
